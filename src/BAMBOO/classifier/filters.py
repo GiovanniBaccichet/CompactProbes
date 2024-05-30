1a229d9aa0a3d9aca286fbf174480f61638227ad
Giovanni Baccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2024-05-30 16:22:03 +0200

    Fixed calculate filter_width function: now counts each character != =
    Signed-off-by: Giovanni Baccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

--- src/BAMBOO/classifier/filters.py
+++ src/BAMBOO/classifier/filters.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-def calculate_filter_width(filter: str) -> int:
-    filter_start, filter_end = filter[0].split(":")
-    width = int(filter_end) - int(filter_start)
-    return width
+def calculate_filter_width(filter_str: str) -> int:
+    return len([char for char in filter_str if char != '0'])
 
 def filter_to_vector(filter_str : str) -> np.ndarray:
     # Convert the string to a list of integers
