Fixed error computation and progress bar
Signed-off-by: Giovanni Baccichet <32038133+GiovanniBaccichet@users.noreply.github.com>

src/BAMBOO/bamboo.py
@@ -94,7 +94,7 @@ def main():
         for _ in range(n_iterations):  # iterations
             # Create a task for the inner loop
             filters_task = progress.add_task(
-                "[green]Processing filters...", total=n_filters
+                "[green]Processing filters...", total=n_processes
             )
 
             chunked_indices = np.array_split(filters.index, n_processes)
@@ -127,7 +127,7 @@ def main():
                         result = future.result()
                         chunk_errors.update(result)  # Collect errors from each future
 
-                        progress.update(filters_task, advance=len(chunk_errors))
+                        progress.update(filters_task, advance=1)
 
                     except Exception as e:
                         utils.logger.log.critical(f"An error occurred: {e}")

src/BAMBOO/classifier/filters.py
@@ -1,7 +1,7 @@
 import numpy as np
+from utils import logger
 
 from . import compute_error
-from utils import logger
 
 
 def calculate_filter_width(filter_str: str) -> int:
@@ -15,19 +15,19 @@ def filter_to_vector(filter_str: str) -> np.ndarray:
     return vector
 
 
-def process_filters_chunk(chunk, string_pair_df, weights) -> dict:
-
+def process_filters_chunk(chunk, string_pair_df, weights, filters_task) -> dict:
     filter_threshold_errors_dict = {}
 
     for _, row in chunk.iterrows():
-
         if chunk.empty:
             logger.log.critical("The input chunk is empty, cannot process filters.")
             raise ValueError("The input chunk is empty, cannot process filters.")
-        
+
         filter = row["filters"]
         thresholds = row["thresholds"]
-        current_errors = compute_error.matrix_error(string_pair_df, thresholds, filter, weights)
+        current_errors = compute_error.matrix_error(
+            string_pair_df, thresholds, filter, weights
+        )
 
         # Merge errors
         for key, value in current_errors.items():